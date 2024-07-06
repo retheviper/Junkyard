8416c6a530cf8620e6747f6f185c0e41ab1d38d6
retheviper <retheviper@gmail.com>
2024-07-06 21:27:17 +0900

    fix dark mode and add exit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,11 +7,10 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
 import androidx.compose.material.NavigationRail
 import androidx.compose.material.NavigationRailItem
+import androidx.compose.material.Surface
 import androidx.compose.material.Switch
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -20,7 +19,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.input.key.Key
+import androidx.compose.ui.input.key.isCtrlPressed
+import androidx.compose.ui.input.key.isMetaPressed
+import androidx.compose.ui.input.key.key
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
@@ -83,29 +85,21 @@ fun MainScreen() {
                 }
             }
 
-            NavHost(
-                navController,
-                startDestination = Screen.Archive.name,
-                enterTransition = { slideInHorizontally() + fadeIn() },
-                exitTransition = { slideOutHorizontally() + fadeOut() }
-            ) {
-                composable(Screen.Archive.name) { ArchiveView() }
-                composable(Screen.RarToZip.name) { RarToZipView() }
-                composable(Screen.ChangeExtension.name) { ChangeExtensionView() }
-                composable(Screen.ImageConvert.name) { ImageConvertView() }
-                composable(Screen.CreateThumbnail.name) { CreateThumbnailView() }
+            Surface {
+                NavHost(
+                    navController,
+                    startDestination = Screen.Archive.name,
+                    enterTransition = { slideInHorizontally() + fadeIn() },
+                    exitTransition = { slideOutHorizontally() + fadeOut() }
+                ) {
+                    composable(Screen.Archive.name) { ArchiveView() }
+                    composable(Screen.RarToZip.name) { RarToZipView() }
+                    composable(Screen.ChangeExtension.name) { ChangeExtensionView() }
+                    composable(Screen.ImageConvert.name) { ImageConvertView() }
+                    composable(Screen.CreateThumbnail.name) { CreateThumbnailView() }
+                }
             }
         }
-    }
-}
-
-@Composable
-private fun MenuButton(onClick: () -> Unit, icon: String, expandedText: String, sidebarWidth: Dp) {
-    Button(
-        onClick = onClick,
-        modifier = Modifier.fillMaxWidth().padding(8.dp)
-    ) {
-        if (sidebarWidth >= 225.dp) Text("$icon $expandedText") else Text(icon)
     }
 }
 
@@ -116,6 +110,11 @@ fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
         title = "🛠️Junkyard",
+        onKeyEvent = { keyEvent ->
+            (keyEvent.isMetaPressed && keyEvent.key == Key.W).also {
+                if (it) exitApplication()
+            }
+        }
     ) {
         window.minimumSize = Dimension(800, 700)
         MainScreen()
